1e13107eaf1049dbc403925ae3c948bee60e104d
mi2valley <monocoin189@gmail.com>
2026-02-03 23:00:28 +0900

    chore: fix cors

--- src/backend/main.py
+++ src/backend/main.py
@@ -36,6 +36,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:4321",
     "http://localhost:5173",
+    "https://qamp-62.github.io",
     "https://qamposer.org",
     "https://www.qamposer.org",
 ]
